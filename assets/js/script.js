80b3ee1b48c09aea188d7a05e735af5fa2561ed4
Emilie Godin-Depaul <em.godin80@yahoo.com>
2023-01-16 14:59:56 +0000

    moved event listeners to bottom

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,99 +66,99 @@ function startQuiz() {
   displayQuestion();
 }
 
-startBtn.addEventListener("click", startQuiz);
 
 // displaying questions
 function displayQuestion() {
     let currentQuestion = questions[currentQuestionIndex];
-  questionTitleEl.textContent = currentQuestion.title;
-
-  choicesEl.innerHTML = "";
-
-  currentQuestion.choices.forEach((choice, i) => {
-    let choiceButton = document.createElement("button");
-    choiceButton.setAttribute("class", "choice");
-    choiceButton.setAttribute("value", choice);
-
-    choiceButton.textContent = `${i + 1}. ${choice}`;
-    choiceButton.onclick = questionChoiceClick;
-
-    choicesEl.appendChild(choiceButton);
-  });
+    questionTitleEl.textContent = currentQuestion.title;
+    
+    choicesEl.innerHTML = "";
+    
+    currentQuestion.choices.forEach(function (choice, i) {
+        let choiceButton = document.createElement("button");
+        choiceButton.setAttribute("class", "choice");
+        choiceButton.setAttribute("value", choice);
+        
+        choiceButton.textContent = `${i + 1}. ${choice}`;
+        choiceButton.onclick = questionChoiceClick;
+        
+        choicesEl.appendChild(choiceButton);
+    });
 }
 
 // clicking question choices
 function questionChoiceClick() {
     // if  user guessed wrong
     if (this.value !== questions[currentQuestionIndex].correctAnswer) {
-      time -= 10;
-      if (time < 0) {
-        time = 0;
-      }
- 
- timerEl.textContent = time;
- feedbackEl.textContent = "Wrong answer!";
- feedbackEl.style.color = "orange";
- feedbackEl.style.fontSize = "50px";
- let wrongAudio = new Audio("./assets/sfx/incorrect.wav");
- wrongAudio.play();
-} else {
- // if user guessed right
- feedbackEl.textContent = "Well done!";
- feedbackEl.style.color = "blue";
- feedbackEl.style.fontSize = "50px";
- let correctAudio = new Audio("./assets/sfx/correct.wav");
- correctAudio.play();
-}
-
-// Display outcome
-feedbackEl.setAttribute("class", "feedback");
-
-setInterval(function () {
- feedbackEl.setAttribute("class", "hide");
+        time -= 10;
+        if (time < 0) {
+            time = 0;
+        }
+        
+        timerEl.textContent = time;
+        feedbackEl.textContent = "Wrong answer!";
+        feedbackEl.style.color = "orange";
+        feedbackEl.style.fontSize = "50px";
+        let wrongAudio = new Audio("./assets/sfx/incorrect.wav");
+        wrongAudio.play();
+    } else {
+        // if user guessed right
+        feedbackEl.textContent = "Well done!";
+        feedbackEl.style.color = "blue";
+        feedbackEl.style.fontSize = "50px";
+        let correctAudio = new Audio("./assets/sfx/correct.wav");
+        correctAudio.play();
+    }
+    
+    // Display outcome
+    feedbackEl.setAttribute("class", "feedback");
+    
+    setInterval(function () {
+        feedbackEl.setAttribute("class", "hide");
 }, 2500);
 // display next question or end quiz
 currentQuestionIndex++;
-  if (currentQuestionIndex === questions.length) {
+if (currentQuestionIndex === questions.length) {
     endQuiz();
-  } else {
+} else {
     displayQuestion();
-  }
+}
 }
 
 // Timer
 function clockTick() {
     time--;
-   timerEl.textContent = time;
-  if (time <= 0) {
-    endQuiz();
-  }
+    timerEl.textContent = time;
+    if (time <= 0) {
+        endQuiz();
+    }
 }
 
 // Ending quiz
 function endQuiz() {
     clearInterval(timerId);
-  endScreenEl.removeAttribute("class");
-  finalScoreEl.textContent = time;
-  questionsEl.setAttribute("class", "hide");
+    endScreenEl.removeAttribute("class");
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute("class", "hide");
 }
 
 // highscores
 function storeHighscores() {
-      let initials = initialsEl.value;
-  
+    let initials = initialsEl.value;
+    
     if (initialsEl !== "") {
-      let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
-      let newScore = {
-        score: time,
-        initials: initials,
-      };
-  
-      highscores.push(newScore);
-      localStorage.setItem("highscores", JSON.stringify(highscores));
-  
-      location.href = "highscores.html";
+        let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+        let newScore = {
+            score: time,
+            initials: initials,
+        };
+        
+        highscores.push(newScore);
+        localStorage.setItem("highscores", JSON.stringify(highscores));
+        
+        location.href = "highscores.html";
     }
-  }
+}
 
-  submitBtn.addEventListener("click", storeHighscores);
+startBtn.addEventListener("click", startQuiz);
+submitBtn.addEventListener("click", storeHighscores);
